665b1685682fc3df6b76a814721fde15b827990a
JuodaanViinaa <87039101+JuodaanViinaa@users.noreply.github.com>
2021-07-07 19:55:00 -0500

    Resumen con convertidor general
    Resumen acoplado con el convertidor generalizado para cualquier número de columnas en el archivo original de Med.
    Se reorganizó el código para que todas las funciones estén declaradas al comienzo del programa.

--- ResumenUltimate.py
+++ ResumenUltimate.py
@@ -20,68 +20,52 @@ columnasLatPal = [2, 7, 12, 17, 22, 27]
 columnasEscapes = [2, 13, 24, 35, 46, 57]
 columnasLatEsc = [2, 13, 24, 35, 46, 57]
 
+
 # Convertidor
-for sesion in range(sesionInicial, sesionFinal + 1):
-    for sujeto in range(len(sujetos)):
-        print('Convirtiendo sesión ' + str(sesion) + ' de sujeto ' + sujetos[sujeto] + '.')
-        # Pandas lee los datos y los escribe en el archivo convertido en 6 columnas separando por los espacios.
-        # El argumento names indica cuántas columnas se crearán. Evita errores cuando se edita el archivo de Med.
-        datos = pandas.read_csv(directorioBrutos + sujetos[sujeto] + '_LIBRES_' + str(sesion), header=None,
-                                names=range(6), sep=r'\s+')
-        datos.to_excel(directorioConvertidos + sujetos[sujeto] + '_LIBRES_' + str(sesion) + '.xlsx',
-                       index=False, header=None)
+def convertir(columnas, subfijo):
+    for ssn in range(sesionInicial, sesionFinal + 1):
+        for sjt in range(len(sujetos)):
+            print('Convirtiendo sesión ' + str(ssn) + ' de sujeto ' + sujetos[sjt] + '.')
+            # Pandas lee los datos y los escribe en el archivo convertido en 6 columnas separando por los espacios.
+            # El argumento names indica cuántas columnas se crearán. Evita errores cuando se edita el archivo de Med.
+            datos = pandas.read_csv(directorioBrutos + sujetos[sjt] + subfijo + str(ssn), header=None,
+                                    names=range(columnas), sep=r'\s+')
+            datos.to_excel(directorioConvertidos + sujetos[sjt] + subfijo + str(ssn) + '.xlsx', index=False,
+                           header=None)
 
-        # Openpyxl abre el archivo creado por pandas, lee la hoja y la almacena en la variable hojaCompleta.
-        archivoCompleto = load_workbook(directorioConvertidos + sujetos[sujeto] + '_LIBRES_' + str(sesion) + '.xlsx')
-        hojaCompleta = archivoCompleto.active
+            # Openpyxl abre el archivo creado por pandas, lee la hoja y la almacena en la variable hojaCompleta.
+            archivoCompleto = load_workbook(directorioConvertidos + sujetos[sjt] + subfijo + str(ssn) + '.xlsx')
+            hojaCompleta = archivoCompleto.active
 
-        # Se genera una lista que contenga sub-listas con todos los valores de las listas dadas por Med.
-        # Funciona para cualquier cantidad de listas.
-        # Los datos se convierten en flotantes para que todos tengan punto decimal, y luego en string para que mas
-        # adelante el método split los pueda separar por el punto.
-        metalista = [[]]
-        contadormetalista = 0
-        columna1 = hojaCompleta['B']
-        columna2 = hojaCompleta['C']
-        columna3 = hojaCompleta['D']
-        columna4 = hojaCompleta['E']
-        columna5 = hojaCompleta['F']
-        for i in range(11, len(columna1)):
-            if columna1[i].value is not None:
-                metalista[contadormetalista].append(str(float(columna1[i].value)))
-                if columna2[i].value is not None:
-                    metalista[contadormetalista].append(str(float(columna2[i].value)))
-                    if columna3[i].value is not None:
-                        metalista[contadormetalista].append(str(float(columna3[i].value)))
-                        if columna4[i].value is not None:
-                            metalista[contadormetalista].append(str(float(columna4[i].value)))
-                            if columna5[i].value is not None:
-                                metalista[contadormetalista].append(str(float(columna5[i].value)))
-            else:
-                metalista.append([])
-                contadormetalista += 1
+            # Se genera una lista que contenga sub-listas con todos los valores de las listas dadas por Med.
+            # Funciona para cualquier cantidad de listas.
+            # Los datos se convierten en flotantes para que todos tengan punto decimal, y luego en string para que mas
+            # adelante el método split los pueda separar por el punto.
+            metalista = [[]]
+            contadormetalista = 0
 
-        # Escribir cada sub-lista en una columna de excel separando por punto decimal.
-        # Se utilizan expresiones regulares (regex) para indicar al programa que debe añadir ceros cuando pandas
-        # los ha eliminado (cuando están al final de una cifra después de un punto decimal).
-        regex1 = re.compile(r'^\d+\.\d{2}$')
-        for i in range(len(metalista)):
-            for j in range(len(metalista[i])):
-                if regex1.search(metalista[i][j]):
-                    metalista[i][j] += '0'
-                hojaCompleta[get_column_letter((i * 2) + 9) + str(j + 1)] = int(metalista[i][j].split('.')[0])
-                hojaCompleta[get_column_letter((i * 2) + 10) + str(j + 1)] = int(metalista[i][j].split('.')[1])
-        archivoCompleto.save(directorioConvertidos + sujetos[sujeto] + '_LIBRES_' + str(sesion) + '.xlsx')
-    print('\n')
+            columna1 = hojaCompleta['B']
+            for fila in range(12, len(columna1)):
+                for columna in range(2, columnas + 1):
+                    if hojaCompleta[get_column_letter(columna) + str(fila)].value is not None:
+                        metalista[contadormetalista].append(str(float(hojaCompleta[get_column_letter(columna) +
+                                                                                   str(fila)].value)))
+                    elif hojaCompleta[get_column_letter(columna) + str(fila)].value is None and columna == 2:
+                        metalista.append([])
+                        contadormetalista += 1
 
-# Resumen
-# Revisar si el archivo de resumen ya existe. De lo contrario, crearlo.
-if archivo in listdir(directorioConvertidos):
-    print('Archivo encontrado. Abriendo...')
-    wb = load_workbook(directorioConvertidos + archivo)
-else:
-    print('Archivo no encontrado. Creando...')
-    wb = Workbook()
+            # Escribir cada sub-lista en una columna de excel separando por punto decimal.
+            # Se utilizan expresiones regulares (regex) para indicar al programa que debe añadir ceros cuando pandas
+            # los ha eliminado (cuando están al final de una cifra después de un punto decimal).
+            regex1 = re.compile(r'^\d+\.\d{2}$')
+            for i in range(len(metalista)):
+                for j in range(len(metalista[i])):
+                    if regex1.search(metalista[i][j]):
+                        metalista[i][j] += '0'
+                    hojaCompleta[get_column_letter((i * 2) + 9) + str(j + 1)] = int(metalista[i][j].split('.')[0])
+                    hojaCompleta[get_column_letter((i * 2) + 10) + str(j + 1)] = int(metalista[i][j].split('.')[1])
+            archivoCompleto.save(directorioConvertidos + sujetos[sjt] + subfijo + str(ssn) + '.xlsx')
+        print('\n')
 
 
 # Función para crear hojas.
@@ -147,6 +131,17 @@ def esccolumnas(titulo, columna, lista, restar):
         for pos in range(len(lista)):
             hojaind[get_column_letter(columna) + str(pos + 2)] = lista[pos]
 
+
+convertir(6, '_LIBRES_')
+
+# Resumen
+# Revisar si el archivo de resumen ya existe. De lo contrario, crearlo.
+if archivo in listdir(directorioConvertidos):
+    print('Archivo encontrado. Abriendo...')
+    wb = load_workbook(directorioConvertidos + archivo)
+else:
+    print('Archivo no encontrado. Creando...')
+    wb = Workbook()
 
 # Crear todas las hojas.
 proporciones = hoja('Proporciones')
